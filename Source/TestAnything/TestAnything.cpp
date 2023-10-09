f789789803caa1ecf63f6b2fe13fdeac5da7479f
Luka <97005447+luqiming666@users.noreply.github.com>
2023-10-09 21:47:12 +0800

    Initial commit

---
+++ Source/TestAnything/TestAnything.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TestAnything.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestAnything, "TestAnything" );
---
+++ Source/TestAnything/TestAnythingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestAnythingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTANYTHING_API ATestAnythingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
